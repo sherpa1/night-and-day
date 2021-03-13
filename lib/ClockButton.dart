3a83f353d6483adbb035a8ee56d00cc1cadbd17a
sherpa1 <alex@sherpa.one>
2021-03-13 14:13:38 +0100

    refactoring + auto time change

--- lib/ClockButton.dart
+++ lib/ClockButton.dart
@@ -9,7 +9,7 @@ class ClockButton extends StatefulWidget {
   }) : super(key: key);
 
   final Function onDayChange; //fired at 18 and
-  final Function onTimeChange; //fired each 6 hours
+  final Function onTimeChange;
   final bool defaultDay; //value set from widget's parent
 
   @override
@@ -18,7 +18,6 @@ class ClockButton extends StatefulWidget {
 
 class _ClockButtonState extends State<ClockButton> {
   bool _aNewDayStarts;
-  int _hour;
 
   @override
   void initState() {
@@ -27,56 +26,38 @@ class _ClockButtonState extends State<ClockButton> {
         if (widget.defaultDay) {
           _aNewDayStarts = widget
               .defaultDay; //start value set from local widget constructor param, given from parent
-          _hour = 6; //local logic
         } else {
           _aNewDayStarts = false;
-          _hour = 0; //local logic
         }
       } else {
         _aNewDayStarts = false;
-        _hour = 0;
       }
     });
-
-    if (widget.defaultDay) {
-      _hour = 0;
-    }
 
     super.initState();
   }
 
   void _onPress() {
-    //each time state is set, build method is executed and all conditionnal displays are re-evaluated
-    setState(() {
-      if (_hour == 18) {
-        _hour = 0;
-        _aNewDayStarts = true;
-        widget.onDayChange(
-          _aNewDayStarts,
-        ); //calling back parent method and give local state value
-      } else {
-        _hour += 6;
-        _aNewDayStarts = false;
-      }
-    });
-
-    widget.onTimeChange(
-      _hour,
-    );
+    widget.onTimeChange(6);
   }
 
   Color _getColor() {
-    return (_aNewDayStarts) ? Colors.yellow[600] : Colors.blueGrey;
+    return (_aNewDayStarts) ? Colors.yellow[600] : Colors.cyan[800];
+  }
+
+  Color _getIconColor() {
+    return (_aNewDayStarts) ? Colors.blueGrey[900] : Colors.white;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      FlatButton(
-        child: Icon(Icons.alarm_add),
-        onPressed: () => _onPress(),
-        color: _getColor(),
+    return FlatButton(
+      child: Icon(
+        Icons.alarm_add,
+        color: _getIconColor(),
       ),
-    ]);
+      onPressed: () => _onPress(),
+      color: _getColor(),
+    );
   }
 }
--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:night_and_day/ClockButton.dart';
+import 'dart:async';
 
 class MainPage extends StatefulWidget {
   MainPage({Key key, this.title}) : super(key: key);
@@ -12,50 +13,51 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   bool _darkMode; //from >=18 to <6
-  int _hour;
+  int _dayMoment;
   DateTime _now;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
     setState(() {
-      var nowHour = DateTime.now().hour;
-
-      if (nowHour >= 0 && nowHour < 6) _hour = 0;
-      if (nowHour >= 6 && nowHour < 12) _hour = 6;
-      if (nowHour >= 12 && nowHour < 18) _hour = 12;
-      if (nowHour >= 18 && nowHour < 0) _hour = 18;
-
       _now = DateTime.now();
 
-      _darkMode = true;
-      _hour = 0;
+      _darkMode = _isDarkMode(_now.hour);
+      _dayMoment = _getDayMoment(_now.hour);
+    });
+
+    Timer.periodic(Duration(seconds: 20), (timer) {
+      //each minute, add an hour
+      setState(() {
+        _onTimeChange(1);
+      });
     });
 
     super.initState();
   }
 
-  void _onDayChange(bool day) {
-    setState(() {
-      _now = _now.add(
-        new Duration(
-          days: 1,
-        ),
-      );
-    });
+  bool _isDarkMode(int hour) {
+    if (hour >= 18 || hour < 6) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  void _onTimeChange(int hour) {
-    setState(() {
-      _hour = hour;
-      if (_hour >= 18 && _hour < 6)
-        _darkMode = true;
-      else
-        _darkMode = false;
-    });
+  int _getDayMoment(int hour) {
+    if (hour < 6)
+      return 0;
+    else if (hour < 12)
+      return 6;
+    else if (hour < 18)
+      return 12;
+    else
+      return 18;
   }
 
   Image _getImage() {
-    switch (_hour) {
+    switch (_dayMoment) {
       case 0:
         return Image(
           width: 250,
@@ -83,8 +85,8 @@ class _MainPageState extends State<MainPage> {
     }
   }
 
-  Color _getColor() {
-    switch (_hour) {
+  Color _getBackgroundColor() {
+    switch (_dayMoment) {
       case 0:
         return Colors.grey.shade900;
         break;
@@ -98,25 +100,29 @@ class _MainPageState extends State<MainPage> {
         return Colors.blueGrey[600];
         break;
       default:
-        return Colors.blueGrey[900];
+        return Colors.grey.shade900;
     }
+  }
+
+  String _getTodayDate() {
+    return _now.toString().split(" ")[0];
   }
 
   Text _getToday() {
     return Text(
-      _now.toString().split(" ")[0],
+      _getTodayDate(),
       style: TextStyle(
         fontFamily: 'FredokaOne',
         fontSize: 30,
-        color: (!_darkMode) ? Colors.blueGrey : Colors.white,
+        color: (_darkMode) ? Colors.white : Colors.cyan[800],
       ),
     );
   }
 
-  Text _getLabel() {
+  Text _getDayMomentText() {
     var label;
 
-    switch (_hour) {
+    switch (_dayMoment) {
       case 0:
         label = "night";
         break;
@@ -124,7 +130,7 @@ class _MainPageState extends State<MainPage> {
         label = "morning";
         break;
       case 12:
-        label = "midday";
+        label = "afternoon";
         break;
       case 18:
         label = "evening";
@@ -137,50 +143,74 @@ class _MainPageState extends State<MainPage> {
       style: TextStyle(
         fontFamily: 'FredokaOne',
         fontSize: 20,
-        color: (!_darkMode) ? Colors.blueGrey : Colors.white,
+        color: (_darkMode) ? Colors.white : Colors.cyan[800],
       ),
     );
   }
 
   Text _getHour() {
-    String _hourStr;
-
-    if (_hour < 10)
-      _hourStr = "0" + _hour.toString();
-    else
-      _hourStr = _hour.toString();
+    var hour =
+        (_now.hour < 10) ? "0" + _now.hour.toString() : _now.hour.toString();
+    var minute = (_now.minute < 10)
+        ? "0" + _now.minute.toString()
+        : _now.minute.toString();
 
     return Text(
-      _hourStr + ':00 o\'clock',
+      hour + ":" + minute,
       style: TextStyle(
         fontFamily: 'FredokaOne',
         fontSize: 20,
-        color: (!_darkMode) ? Colors.blueGrey : Colors.white,
+        color: (_darkMode) ? Colors.white : Colors.cyan[800],
       ),
     );
+  }
+
+  void _onDayChange(bool day) {
+    setState(() {
+      _now = _now.add(
+        new Duration(
+          days: 1,
+        ),
+      );
+    });
+
+    final snackBar = SnackBar(content: Text("Today is ${_getTodayDate()}"));
+    //Scaffold.of(context).showSnackBar(snackBar);//can't use classic syntax because we are outside build method
+    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
+
+  void _onTimeChange(int hoursToAdd) {
+    setState(() {
+      _now = _now.add(Duration(hours: hoursToAdd));
+      _dayMoment = _getDayMoment(_now.hour);
+      _darkMode = _isDarkMode(_now.hour);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      key:
+          _scaffoldKey, //store context for using it inside _onCharacterRemoved method and showing SnackBar
+
+      body: AnimatedContainer(
+        // Define how long the animation should take.
+        duration: Duration(seconds: 1),
+        // Provide an optional curve to make the animation feel smoother.
+        curve: Curves.fastOutSlowIn,
         width: double.infinity, //full width
-        color: _getColor(),
+        color: _getBackgroundColor(),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             Column(
               children: [
                 _getToday(),
-                _getLabel(),
+                _getDayMomentText(),
                 _getHour(),
               ],
             ),
-            AnimatedOpacity(
-              opacity: (_hour == 0) ? 0.0 : 1.0,
-              duration: Duration(milliseconds: 800),
-              child: _getImage(),
-            ),
+            _getImage(),
             ClockButton(
               onDayChange: _onDayChange,
               onTimeChange: _onTimeChange,
